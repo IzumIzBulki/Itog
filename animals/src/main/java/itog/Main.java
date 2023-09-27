4fdd1a4e81014fd3f8d3e0bd0da52d4eeef8c39f
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2023-09-27 13:31:39 +0300

    Create Main.java

---
+++ animals/src/main/java/itog/Main.java
@@ -0,0 +1,14 @@
+package itog;
+
+import itog.controller.AnimalController;
+import itog.services.AnimalList;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AnimalController controller = new AnimalController();
+
+        controller.Run();
+    }
+}
